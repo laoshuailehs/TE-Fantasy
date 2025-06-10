ed12c15f7a49bab0eb1b4aec16f9e3857ab8b6c9
wain <962996519@qq.com>
2025-06-10 16:45:57 +0800

    redis

---
+++ GameLoginServer/RedisServer/RedisServer/RedisServer/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace RedisServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.connect = new System.Windows.Forms.Button();
+            this.StartServer = new System.Windows.Forms.Button();
+            this.selectdb = new System.Windows.Forms.Button();
+            this.searsh1 = new System.Windows.Forms.TextBox();
+            this.stopserver = new System.Windows.Forms.Button();
+            this.ip = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.port = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(52, 214);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(377, 224);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(52, 127);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(144, 48);
+            this.connect.TabIndex = 1;
+            this.connect.Text = "连接Redis";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // StartServer
+            // 
+            this.StartServer.Location = new System.Drawing.Point(52, 29);
+            this.StartServer.Name = "StartServer";
+            this.StartServer.Size = new System.Drawing.Size(144, 48);
+            this.StartServer.TabIndex = 2;
+            this.StartServer.Text = "启动服务器";
+            this.StartServer.UseVisualStyleBackColor = true;
+            this.StartServer.Click += new System.EventHandler(this.StartServer_Click);
+            // 
+            // selectdb
+            // 
+            this.selectdb.Location = new System.Drawing.Point(228, 127);
+            this.selectdb.Name = "selectdb";
+            this.selectdb.Size = new System.Drawing.Size(144, 48);
+            this.selectdb.TabIndex = 3;
+            this.selectdb.Text = "查找数据";
+            this.selectdb.UseVisualStyleBackColor = true;
+            this.selectdb.Click += new System.EventHandler(this.selectdb_Click);
+            // 
+            // searsh1
+            // 
+            this.searsh1.Location = new System.Drawing.Point(228, 187);
+            this.searsh1.Name = "searsh1";
+            this.searsh1.Size = new System.Drawing.Size(144, 21);
+            this.searsh1.TabIndex = 4;
+            // 
+            // stopserver
+            // 
+            this.stopserver.Location = new System.Drawing.Point(228, 29);
+            this.stopserver.Name = "stopserver";
+            this.stopserver.Size = new System.Drawing.Size(144, 48);
+            this.stopserver.TabIndex = 5;
+            this.stopserver.Text = "关闭服务器";
+            this.stopserver.UseVisualStyleBackColor = true;
+            this.stopserver.Click += new System.EventHandler(this.stopserver_Click);
+            // 
+            // ip
+            // 
+            this.ip.Location = new System.Drawing.Point(80, 90);
+            this.ip.Name = "ip";
+            this.ip.Size = new System.Drawing.Size(116, 21);
+            this.ip.TabIndex = 6;
+            this.ip.Text = "127.0.0.1";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(45, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 23);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ip";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // port
+            // 
+            this.port.Location = new System.Drawing.Point(256, 90);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(116, 21);
+            this.port.TabIndex = 8;
+            this.port.Text = "8019";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(221, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 23);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "port";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ip);
+            this.Controls.Add(this.stopserver);
+            this.Controls.Add(this.searsh1);
+            this.Controls.Add(this.selectdb);
+            this.Controls.Add(this.StartServer);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox port;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.TextBox ip;
+
+        private System.Windows.Forms.Button stopserver;
+
+        private System.Windows.Forms.TextBox searsh1;
+
+        private System.Windows.Forms.Button selectdb;
+
+        private System.Windows.Forms.Button StartServer;
+
+        private System.Windows.Forms.Button connect;
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+
+        #endregion
+    }
+}
---
+++ GameLoginServer/RedisServer/RedisServer/RedisServer/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using StackExchange.Redis;
+
+namespace RedisServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private Socket serverSocket;
+        private ConnectionMultiplexer redisConnection;
+        private IDatabase redisDatabase;
+
+        private void ConnectRedis()
+        {
+            string redisConnectionString = "127.0.0.1:6379";
+            try
+            {
+                redisConnection = ConnectionMultiplexer.Connect(redisConnectionString);
+                redisDatabase = redisConnection.GetDatabase();
+                AddText("Redis 连接成功");
+            }
+            catch (Exception e)
+            {
+                AddText("Redis 连接失败: " + e.Message);
+            }
+        }
+
+        private void SearchRedisData(string key)
+        {
+            if (redisDatabase == null)
+            {
+                AddText("请先连接 Redis");
+                return;
+            }
+            try
+            {
+                RedisType type = redisDatabase.KeyType(key);
+                switch (type)
+                {
+                    case RedisType.String:
+                        string value = redisDatabase.StringGet(key);
+                        AddText($"[String] {key} = {value}");
+                        break;
+                    case RedisType.Hash:
+                        var hash=redisDatabase.HashGetAll(key);
+                        foreach (var item in hash)
+                        {
+                            AddText($"[Hash]{key} {item.Name} = {item.Value}");
+                        }
+                        break;
+                    case RedisType.List:
+                        var list = redisDatabase.ListRange(key);
+                        foreach (var item in list)
+                        {
+                            AddText($"[List]{key} {item}");
+                        }
+                        break;
+                    case RedisType.Set:
+                        var set = redisDatabase.SetMembers(key);
+                        foreach (var item in set)
+                        {
+                            AddText($"[Set]{key} {item}");
+                        }
+                        break;
+                    case RedisType.SortedSet:
+                        var sortedSet = redisDatabase.SortedSetRangeByRank(key);
+                        for (int i = 0; i < sortedSet.Length; i++)
+                        {
+                            AddText($"[SortedSet] {key}[{i}] = {sortedSet[i]} | Score: {sortedSet[i]}");
+                        }
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                AddText("Redis 查询失败: " + e.Message);
+            }
+            
+        }
+        
+        private void SearchRedisHylogData(string key)
+        {
+            if (redisDatabase == null)
+            {
+                AddText("请先连接 Redis");
+                return;
+            }
+            try
+            {
+                var value = redisDatabase.HyperLogLogLength(key);
+                AddText($"[HyperLogLog] {key} 的唯一元素数量 = {value}");
+            }
+            catch  (Exception e)
+            {
+                AddText("Redis 搜索失败: " + e.Message);
+            }
+        }
+        private void SearchRedisBitData(string key, long offset)
+        {
+            if (redisDatabase == null)
+            {
+                AddText("请先连接 Redis");
+                return;
+            }
+
+            try
+            {
+                var value = redisDatabase.StringGetBit(key,offset);
+                AddText($"[Bitmap] {key} 偏移量 [{offset}] 的值为: {(value ? 1 : 0)}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        private void SearchRedisEgoData(string key)
+        {
+            if (redisDatabase == null)
+            {
+                AddText("请先连接 Redis");
+                return;
+            }
+            try
+            {
+                var value = redisDatabase.SortedSetRangeByRank(key);
+                if (value.Length == 0)
+                {
+                    AddText($"[Geospatial] {key}  中无地理数据");
+                    return;
+                }
+                foreach (var item in value)
+                {
+                    var position=redisDatabase.GeoPosition(key,item);
+                    if (position.HasValue)
+                    {
+                        AddText($"[Geospatial] 地点: {item} | 经纬度: {position.Value.Longitude},{position.Value.Latitude}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                AddText("Redis 搜索失败: " + e.Message);
+            }
+        }
+        
+        private void StopServerConnect()
+        {
+            if (serverSocket != null)
+            {
+                serverSocket.Close();
+                serverSocket = null;
+                AddText("服务器已关闭");
+            }
+        }
+        
+        private void StartServerConnect()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ip = IPAddress.Parse(this.ip.Text);
+            int port=int.Parse(this.port.Text);
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            serverSocket.Bind(endPoint);
+            serverSocket.Listen(10);
+            AddText("服务器启动成功,等待连接。。。");
+            serverSocket.BeginAccept(AcceptClient, serverSocket);
+        }
+
+        private void AcceptClient(IAsyncResult result)
+        {
+            
+        }
+        
+        
+        private void AddText(string text)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.Invoke(new Action<string>(AddText), text);
+            }
+            else
+            {
+                richTextBox1.AppendText(text + "\r\n");
+                richTextBox1.ScrollToCaret(); // 这里必须也在 UI 线程执行
+            }
+        }
+
+        private void StartServer_Click(object sender, EventArgs e)
+        {
+            StartServerConnect();
+        }
+        
+        private void connect_Click(object sender, EventArgs e)
+        {
+            ConnectRedis();
+        }
+
+        private void selectdb_Click(object sender, EventArgs e)
+        {
+            //RedisType已有类型 string hash set sortedset(zset) list
+            SearchRedisData(searsh1.Text);
+            
+            //HyperLogLog类型
+            // SearchRedisHylogData(searsh1.Text);
+            
+            //Bitmap类型
+            // var array=searsh1.Text.Split(',');
+            // long.TryParse(array[1],out long offset);
+            // SearchRedisBitData(array[0],offset);
+            
+            //Geospatial类型
+            // SearchRedisEgoData(searsh1.Text);
+        }
+
+        private void stopserver_Click(object sender, EventArgs e)
+        {
+            StopServerConnect();
+        }
+        
+    }
+}
